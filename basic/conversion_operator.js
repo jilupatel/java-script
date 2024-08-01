93536faf860f6111eb6a93149c5c0967625a6a74
Jilu RohitKumar Patel <113164591+jilupatel@users.noreply.github.com>
2024-08-01 16:11:09 +0000

    conversion of data type

---
+++ basic/conversion_operator.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//null => 0
+//undifined => NaN
+//true => 1
+//false => 0
+
+let isLoggedIn = "Jilu"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true
+//0 => false
+//"" => false
+//"jilu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
--- basic/variable.js
+++ basic/variable.js
@@ -6,6 +6,7 @@ let e
 
 // a=21;
 console.log(a);
+d="hey, my dear friend"
 console.table([a,b,c,d,e])
 console.log(typeof null) //type object
 console.log(typeof undifined) //type undifined
